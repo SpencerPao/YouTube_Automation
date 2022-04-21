b931b2277bb77140229d81f641b90818c761a67a
Spencer <Spaopaopao@gmail.com>
2022-04-21 19:43:05 -0400

    updating init

--- ytad/__init__.py
+++ ytad/__init__.py
@@ -1,3 +1,5 @@
-
+from . import app_config, authentication, download_my_uploads, new_description, update_description
+__all__ = ["app_config", "authentication", "download_my_uploads",
+           "new_description", "update_description"]
 from . import _version
 __version__ = _version.get_versions()['version']
